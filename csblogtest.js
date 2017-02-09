771136ab42aab2af3ae3aed4112cd395080036aa
CSI003 <merit_abshire@davincischools.org>
2017-02-09 15:44:21 -0800

    Update csblogtest.js

--- csblogtest.js
+++ csblogtest.js
@@ -4,4 +4,6 @@ $('.menu-bar').on('click', function(){
 	$('nav ul').toggleClass('reveal');
 });
 
+// code for menu bar reveal - directs for the menu to open when clicked //
+
 // BLOG FORMAT END //
